8803bc807b7596573938533e8486d674408d867c
Valdjr <valdirdallagojr@gmail.com>
2022-08-15 21:49:18 -0300

    update deliveryman in delivery

--- src/routes.ts
+++ src/routes.ts
@@ -6,6 +6,7 @@ import { AuthenticateDeliverymanController } from "./modules/account/authenticat
 import { CreateClientController } from "./modules/clients/useCases/createClient/CreateClientController"
 import { CreateDeliveryController } from "./modules/deliveries/useCases/createDelivery/CreateDeliveryController"
 import { FindAllAvailableController } from "./modules/deliveries/useCases/findAllAvailable/FindAllAvailableController"
+import { UpdateDeliverymanController } from "./modules/deliveries/useCases/updateDelivery/UpdateDeliverymanController"
 import { CreateDeliverymanController } from "./modules/deliveryman/useCases/createDeliveryman/CreateDeliverymanController"
 
 const routes = Router()
@@ -15,6 +16,7 @@ const createClientController = new CreateClientController()
 const createDeliverymanController = new CreateDeliverymanController()
 const createDeliveryController = new CreateDeliveryController()
 const findAllAvailableController = new FindAllAvailableController()
+const updateDeliverymanController = new UpdateDeliverymanController()
 
 routes.post("/client", createClientController.handle)
 routes.post("/client/authenticate", authenticateClientController.handle)
@@ -22,5 +24,6 @@ routes.post("/deliveryman", createDeliverymanController.handle)
 routes.post("/deliveryman/authenticate", authenticateDeliverymanController.handle)
 routes.post("/delivery", ensureAuthenticateClient, createDeliveryController.handle)
 routes.get("/delivery/available", ensureAuthenticateDeliveryman, findAllAvailableController.handle)
+routes.put("/delivery/updateDeliveryman/:id", ensureAuthenticateDeliveryman, updateDeliverymanController.handle)
 
 export { routes }
